5170ef4b4cf9ba721e75f58e609e7dfdc6c2eba3
Masson <l.masson@rns.ecolescreatives.com>
2021-03-10 14:43:27 +0100

    Classement de course

---
+++ Course.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+class Course {
+    private: 
+    std::string _identifiant;
+    int _score;
+
+    public:
+    std::string affichePseudo();
+    float calculMoyenne();
+    int afficheScore();
+    void ajouteScore(); 
+    Course(std::string identifiant, int score)
+};
