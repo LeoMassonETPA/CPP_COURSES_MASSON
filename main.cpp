a56cdb04d63bd17f603565d6fbd5b5a4cd75dbdf
Masson <l.masson@rns.ecolescreatives.com>
2021-03-10 15:15:22 +0100

    Course

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Course.cpp"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    Course course1 = Course("Valée de la mort"; "-1");
+    Course course2 = Course("Baby Park"; "-1");
+    Course course3 = Course("RainbowRace"; "-1");
+    Course course4 = Course("BowserCastillo"; "-1");
+    Course course5 = Course("Long-fleuve-tranquille"; "-1");
+}
