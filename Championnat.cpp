b852eb06ef40fe47812412926b7c02125082dc8c
Masson <l.masson@rns.ecolescreatives.com>
2021-03-10 16:25:04 +0100

    Update Championnat.cpp

--- Championnat.cpp
+++ Championnat.cpp
@@ -2,23 +2,6 @@
 #include <iostream>
 #include <string>
 
-/*class Championnat{
-    private:
-    int _nbCourses;
-    int _meilleurScore;
-    float _tempsMoyen;
-    int _meilleurTpsToutConf;
-    int _meilleurTpsJoueur;
-    int _meilleurTpsTotal;
-
-    public:
-    int afficheMeilleurScore();
-    float afficheTempsMoyen();
-    int afficheMeilleursTpsToutConf();
-    int afficheMeilleurTpsJoueur();
-    int afficheMeilleurTpsTotal();
-    Championnat(int nbCourses, float tempsMoyen, int meilleurScore, int meilleursTpsToutConf, int meilleurTpsJoueur, int meilleurTpsTotal);
-}; */
 
 int Championnat::afficheMeilleurScore(){
     return _meilleurscore;
@@ -32,4 +15,10 @@ int Championnat::afficheMeilleurTpsToutConf(){
     return _meilleurTpsToutConf;
 }
 
-int 
+int Championnat::afficheMeilleurTpsJoueur(){
+    return _meilleurTpsJoueur;
+}
+
+int Championnat::afficheMeilleurTpsTotal(){
+    return _meilleurTpsTotal;
+}
