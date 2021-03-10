bd9632226f23174dae0e687966499132bb4e32ef
Masson <l.masson@rns.ecolescreatives.com>
2021-03-10 15:58:58 +0100

    Course

---
+++ Championnat.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+class Championnat{
+    private:
+    int _nbCourses;
+    int _meilleurScore;
+    float _tempsMoyen;
+    int _meilleurTpsToutConf;
+    int _meilleurTpsJoueur;
+    int _meilleurTpsTotal;
+
+    public:
+    int afficheMeilleurScore();
+    float afficheTempsMoyen();
+    int afficheMeilleursTpsToutConf();
+    int afficheMeilleurTpsJoueur();
+    int afficheMeilleurTpsTotal();
+    Championnat(int nbCourses, float tempsMoyen);
+};
